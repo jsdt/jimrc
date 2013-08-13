6cf7e838705682a8b7b7b8872d5db3c3041e7b9d
Jeffrey DallaTezza <dallatez@amazon.com>
2013-08-13 13:49:45 -0700

    Adding error handling.

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -Sso ~/.vim/autoload/pathogen.vim \
 	https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
+if [ $? -ne 0 ]
+then
+    echo "Error downloading pathogen"
+    exit 1
+fi
+
 touch ~/.vimrc
 if ! grep -Fxq "execute pathogen#infect()" ~/.vimrc
 then
@@ -18,6 +24,17 @@ fi
 cd ~/.vim/bundle
 #Download nerdtree
 git clone https://github.com/scrooloose/nerdtree.git
+
+if [ $? -ne 0 ]
+then
+    echo "Error cloning nerdtree"
+    exit 1
+fi
 #Download ctrlp
 git clone https://github.com/kien/ctrlp.vim
 
+if [ $? -ne 0 ]
+then
+    echo "Error cloning ctrlp"
+    exit 1
+fi
